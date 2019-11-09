b49ccca2c20dd31bf3f83b644990d5e00e244a73
Zhenxi <PiscesXP@outlook.com>
2019-11-09 10:51:51 +0800

    修改登录API返回格式为统一格式

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const router = express.Router({});
 const auth = require('../util/auth');
 const dataUtil = require('../util/dataUtil');
 const bodyParser = require('body-parser');
+const resultWrapper = require("../util/resultWrapper")
 
 const getToken = auth.getToken;
 
@@ -15,21 +16,22 @@ module.exports = router;
 router.use('/login', bodyParser.json());
 router.use('/login', bodyParser.urlencoded({extended: true}));
 router.post('/login', async (req, res, next) => {
-    const id = req.body.id;
+    const loginName = req.body.loginName;
     const password = req.body.password;
-    console.log(id)
-    if (id === undefined || id === '' || password === undefined || password === '') {
+    console.log(`on /login:${loginName},${password}`)
+    if (loginName === undefined || loginName === '' || password === undefined || password === '') {
         return next(new Error("Wrong Parameter"));
     } else {
-        const role = await dataUtil.checkLogin(id, password);
+        const role = await dataUtil.checkLogin(loginName, password);
         req.auth = {
-            account: req.body.id,
+            account: req.body.loginName,
             auth: role,
         };
         getToken(req, res, next);
-        res.json({
-            success: role !== undefined
-        });
+        if(role!==undefined){
+            //TODO 修改权限
+            res.json(resultWrapper.wrapSuccess())
+        }
     }
 });
 
