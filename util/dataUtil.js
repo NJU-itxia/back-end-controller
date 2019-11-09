5310c4d450844787e071fa024768f08e6672f1ec
Zhenxi <PiscesXP@outlook.com>
2019-11-09 09:44:27 +0800

    修改登录验证：新数据库表、加盐密码

--- util/dataUtil.js
+++ util/dataUtil.js
@@ -1,6 +1,9 @@
 const mysql = require('mysql');
 const config = require('../config/config');
 const axios = require('axios');
+const crypto = require('crypto');
+
+const salt = config.password.salt;
 
 /**
  * 初始化数据库连接池
@@ -23,22 +26,39 @@ const dataUtil = exports = module.exports = {};
 /**
  * 检查后台账号登陆是否成功
  * property checkLogin is not defined in type(解决这个)
- * @param username 用户名
+ * @param loginName 用户名
  * @param password 密码
  * @param next 回调方法
  * @returns {Promise<any>}
  */
-dataUtil.checkLogin = (username, password) => {
-    const queryStr = 'select account, password, admin from members where account= ?';
+dataUtil.checkLogin = (loginName, password) => {
+    const queryStr = 'select password, role from members where login_name= ?';
     return new Promise((resolve, reject) => {
         pool.getConnection((err, connection) => {
             if (err) {
                 reject(err);
             } else {
-                connection.query(queryStr, [username], (err, rows) => {
+                connection.query(queryStr, [loginName], (err, rows) => {
                     if (err) {
                         reject(err);
                     } else {
+                        switch(rows.length){
+                            case 0:
+                                reject("无此用户")
+                                break;
+                            case 1:
+                                const userRow = rows[0];
+                                const sha256 = crypto.createHash("sha256");
+                                if(sha256.digest(sha256.digest(password).concat(salt))===userRow.password){
+                                    resolve(userRow.role);  //返回角色
+                                }
+                                else{
+                                    reject("密码不正确")
+                                }
+                                break;
+                            default:
+                                reject("登录名有重复")  //不应该出现
+                        }
                         let result;
                         if (rows && rows[0] && rows[0].password && rows[0].password === password) {
                             const row = rows[0];
@@ -53,6 +73,10 @@ dataUtil.checkLogin = (username, password) => {
     })
 };
 
+
+/**
+ * 作用暂不明确
+ */
 dataUtil.checkUserLogin = (username, password) => {
     return new Promise(((resolve, reject) => {
         axios.post(
