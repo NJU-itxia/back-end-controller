ba8cc1c9504d1887be4485743be4296e2eedd1d1
Zhenxi <PiscesXP@outlook.com>
2019-11-10 08:42:33 +0800

    OPTIONS请求直接返回，不再next路由

--- app.js
+++ app.js
@@ -15,16 +15,20 @@ const authCheck = auth.authCheck;
 
 const app = express();
 
-const allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Content-Type', 'application/json;charset=utf-8');
-    next();
-};
-
-app.use(allowCrossDomain);
+app.use(function (req, res, next) {
+    //处理OPTIONS请求
+    if(req.method==="OPTIONS"){
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Content-Type', 'application/json;charset=utf-8');
+        res.send();
+    }
+    else{
+        next();
+    }
+});
 app.use(logger('dev'));
 // app.use(express.json());
 app.use(cookieParser());
